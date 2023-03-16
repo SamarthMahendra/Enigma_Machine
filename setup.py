50f642544cf24a400107d48b7d8acb039694efa7
Samarth Mahendra <samarth.mahendragowda@gmail.com>
2023-03-17 00:14:52 +0530

    inital commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enigma_machine",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="M Samarth Mahendra",
+    author_email="samarth.mahendragowda@gmail.com",
+    description="A simple Enigma machine implementation",
+    url="https://github.com/yourusername/enigma_machine",
+)
